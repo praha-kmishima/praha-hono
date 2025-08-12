49e1d000a789d6a9267947030c451e65608af130
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-08-12 13:37:55 +0000

    add: GET,POSTリクエストの動作

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({ text: "hello world" }, 200)
+})
+
+app.post('/', async (c) => {
+  if (c.req.header('content-type') !== 'application/json') {
+    return c.text('Bad Request', 400)
+  }
+  const body = await c.req.json()
+  return c.json(body, 201)
 })
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 8080
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
